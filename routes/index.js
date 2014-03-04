070977288b6e29cf5dd413050a6578fd20feee8b
hubharani <hubharani@ebay.com>
2014-03-04 14:31:58 -0800

    first major checkin:
    got the ui up
    got end to end working

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ exports.index = function(req, res) {
     res.render('index', {
         title: 'Request Generator'
     });
-    var url = req.body.url || 'http://ebay.com';
-    var num = req.body.num || 50;
-    genRequest(url, num);
+    // var url = req.body.url || 'http://ebay.com';
+    // var num = req.body.num || 50;
+    // genRequest(url, num);
 };
